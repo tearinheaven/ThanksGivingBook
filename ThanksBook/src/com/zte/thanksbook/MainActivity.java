88263af81c960d213dfe04816e4134f682a413ad
lonsy <weixiongcqu@gmail.com>
2014-03-08 17:40:56 +0800

    提交菜单遮罩

--- ThanksBook/src/com/zte/thanksbook/MainActivity.java
+++ ThanksBook/src/com/zte/thanksbook/MainActivity.java
@@ -3,19 +3,26 @@ package com.zte.thanksbook;
 import android.app.Activity;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
-import android.graphics.Color;
+import android.content.Context;
+import android.graphics.PixelFormat;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.ViewGroup.LayoutParams;
+import android.view.WindowManager;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
     
      private ImageButton slipButton;
      private ImageButton indexButton;
      private ImageView img;
+     
+     WindowManager manager;
     
     
     @Override
@@ -23,18 +30,35 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         this.getActionBar().hide();
+        
+        manager = (WindowManager)getSystemService(Context.WINDOW_SERVICE); 
+        
         slipButton = (ImageButton)this.findViewById(R.id.main_slip);
         indexButton = (ImageButton)this.findViewById(R.id.main_app);
         //点击首页图标事件
         slipButton.setOnClickListener(new OnClickListener(){
                @Override
                public void onClick(View arg0) {
+            	   /*WindowManager.LayoutParams lp = new WindowManager.LayoutParams(  
+            			   LayoutParams.FILL_PARENT, LayoutParams.FILL_PARENT,  
+            			   WindowManager.LayoutParams.TYPE_APPLICATION,  
+            			   WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE  
+            			   | WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,  
+            			   PixelFormat.TRANSLUCENT);  
+            			     
+            			   lp.gravity = Gravity.BOTTOM;// 可以自定义显示的位置  
+            			   lp.y = 10;   
+            			   View mNightView = new TextView(MainActivity.this);  
+            			   mNightView.setBackgroundColor(0x80000000);  
+            			   manager.addView(mNightView, lp);*/
+            	   
                     img = (ImageView)findViewById(R.id.ima);
                     NavigationFragment navigation = new NavigationFragment();
                     FragmentManager fragManager = getFragmentManager();
                     FragmentTransaction tran = fragManager.beginTransaction();
                     //tran.add(navigation, null);
                     tran.replace(R.id.content, navigation);
+                    tran.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
                     //img.setBackgroundColor(Color.parseColor("#86222222"));
                     tran.addToBackStack(null);
                     tran.commit();
--- ThanksBook/src/com/zte/thanksbook/NavigationFragment.java
+++ ThanksBook/src/com/zte/thanksbook/NavigationFragment.java
@@ -1,17 +1,48 @@
 package com.zte.thanksbook;
 
+import android.app.Activity;
 import android.app.Fragment;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class NavigationFragment extends Fragment{
+	private Activity myActivity;
 
      @Override
      public View onCreateView(LayoutInflater inflater, ViewGroup container,
                Bundle savedInstanceState) {
-          return inflater.inflate(R.layout.navigation, container,false);
+          View v = inflater.inflate(R.layout.navigation, container,false);
+          
+          this.myActivity = this.getActivity();
+          
+          Button b = (Button)v.findViewById(R.id.shadow_text_view);
+          b.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+
+		    	 FragmentTransaction ft = myActivity.getFragmentManager().beginTransaction();
+		    	 
+		    	 ft.remove(NavigationFragment.this).commit();
+			}
+		});
+          
+          return v;
      }
     
+     
+     public void closeMenu(View view)
+     {
+    	 Log.v(null, "111");
+    	 
+    	 FragmentTransaction ft = this.getActivity().getFragmentManager().beginTransaction();
+    	 
+    	 ft.remove(this).commit();
+     }
 }
