7444f69087460b1aade4fac763a061bad7f4c541
lonsy <weixiongcqu@gmail.com>
2014-04-09 22:48:30 +0800

    发布语音暂停界面提交

--- ThanksBook/src/com/zte/thanksbook/activities/NewAudioMessageActivity.java
+++ ThanksBook/src/com/zte/thanksbook/activities/NewAudioMessageActivity.java
@@ -9,16 +9,25 @@ import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.Animation.AnimationListener;
 import android.view.animation.AnimationUtils;
-import android.widget.ImageButton;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.zte.thanksbook.R;
 
 public class NewAudioMessageActivity extends Activity {
+	//是否按住录音按钮
 	private boolean isTouching = false;
+	//是否有录音文件
+	private boolean hasAudio = false;
 	
 	private View micActionLittle;
 	
 	private View micActionBig;
+	
+	private Button micBtn;
+	
+	private TextView micTip;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -26,10 +35,13 @@ public class NewAudioMessageActivity extends Activity {
 		setContentView(R.layout.activity_new_audio_message);
 		this.getActionBar().hide();
 		
-		this.findViewById(R.id.mic_btn).setOnTouchListener(onMicBtnListener);
+		this.micBtn = (Button)this.findViewById(R.id.mic_btn);
+		micBtn.setOnTouchListener(onMicBtnListener);
 		
 		this.micActionLittle = NewAudioMessageActivity.this.findViewById(R.id.mic_action_little);
 		this.micActionBig = NewAudioMessageActivity.this.findViewById(R.id.mic_action_big);
+		
+		this.micTip = (TextView)this.findViewById(R.id.mic_tip);
 	}
 
 	private View.OnTouchListener onMicBtnListener = new View.OnTouchListener() {
@@ -39,7 +51,16 @@ public class NewAudioMessageActivity extends Activity {
 				Log.v(null, "按下录音按钮");
 				isTouching = true;
 				
-				ImageButton micInner = (ImageButton)NewAudioMessageActivity.this.findViewById(R.id.mic_btn);
+				micBtn.setBackgroundResource(R.drawable.mic_recording);
+				micBtn.setText("");
+				micTip.setText("松开暂停");
+				
+				NewAudioMessageActivity.this.findViewById(R.id.mic_pause_big).setVisibility(View.INVISIBLE);
+				NewAudioMessageActivity.this.findViewById(R.id.mic_re_record).setVisibility(View.INVISIBLE);
+				((TextView)NewAudioMessageActivity.this.findViewById(R.id.mic_play_tip)).setText("");
+				((TextView)NewAudioMessageActivity.this.findViewById(R.id.mic_record_second)).setText("");
+								
+				ImageView micInner = (ImageView)NewAudioMessageActivity.this.findViewById(R.id.mic_view);
 				micInner.setBackgroundResource(R.drawable.mic_action_inner);
 				
 				micActionLittle.setVisibility(View.VISIBLE);
@@ -49,12 +70,24 @@ public class NewAudioMessageActivity extends Activity {
 			}
 			else if (event.getAction() == KeyEvent.ACTION_UP) {
 				Log.v(null, "松开录音按钮");
-				isTouching = false;
+				isTouching = false;				
+
+				micBtn.setBackgroundResource(R.drawable.border_corner_mic);
+				micBtn.setText("按住继续录音");
+				micTip.setText("");
 				
 				micActionLittle.setVisibility(View.INVISIBLE);
 				micActionBig.setVisibility(View.INVISIBLE);
-				ImageButton micInner = (ImageButton)NewAudioMessageActivity.this.findViewById(R.id.mic_btn);
-				micInner.setBackgroundResource(R.drawable.mic_init);
+				
+				hasAudio = true;
+				ImageView micInner = (ImageView)NewAudioMessageActivity.this.findViewById(R.id.mic_view);
+				micInner.setBackgroundResource(R.drawable.mic_play);
+				
+				NewAudioMessageActivity.this.findViewById(R.id.mic_pause_big).setVisibility(View.VISIBLE);
+				NewAudioMessageActivity.this.findViewById(R.id.mic_re_record).setVisibility(View.VISIBLE);
+				((TextView)NewAudioMessageActivity.this.findViewById(R.id.mic_play_tip)).setText("点击按钮播放");
+				((TextView)NewAudioMessageActivity.this.findViewById(R.id.mic_record_second)).setText("14''");
+				
 			}
 			return false;
 		}
