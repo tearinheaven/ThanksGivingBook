01cb4a56902f5c01cc166426234c0ba40c417b84
tearinheaven <hjxwww2008@gmail.com>
2014-04-13 01:02:09 +0800

    新建文本信息回退
    新建文本信息回退
    新建文本信息数据类

--- ThanksBook/src/com/zte/thanksbook/db/SQLiteHelper.java
+++ ThanksBook/src/com/zte/thanksbook/db/SQLiteHelper.java
@@ -9,14 +9,17 @@ public class SQLiteHelper extends SQLiteOpenHelper {
 	private static final String DATABASE_NAME = "ThanksGiving.db";
 	
 	private static final int DATABASE_VERSION = 1;
+	
+	private String tableCreateSql;
 
-    public SQLiteHelper(Context context) {
+    public SQLiteHelper(Context context,String tableCreateSql) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        this.tableCreateSql = tableCreateSql;
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL(UserDao.USER_TABLE_CREATE);
+        db.execSQL(tableCreateSql);
         /*String[] names = MainActivity.resources.getStringArray(R.array.activity_names);
         String[] types = MainActivity.resources.getStringArray(R.array.activity_types);
         for (int i=0; i<names.length; i++)
